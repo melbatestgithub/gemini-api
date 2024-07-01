533aadaf47cde56da3285149ff7117f9bf3fd724
melakuzeleke <melakuzeleke443@gmail.com>
2024-07-01 22:42:22 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
+import Body from './components/Body';
+import SideMenu from './components/SideMenu';
+import Bottom from './components/Bottom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <p>Gerami API</p>
+     <Body/>
+     <SideMenu/>
+     <Bottom/>
     </div>
   );
 }
