d738a0f79832c8c4df3519bb91cb5ba40bda38cc
melakuzeleke <melakuzeleke443@gmail.com>
2024-07-01 22:40:50 +0300

    commit

---
+++ src/components/Bottom.js
@@ -0,0 +1,12 @@
+// src/components/Bottom.js
+import React from 'react';
+
+function Bottom() {
+  return (
+    <footer className="bg-gray-900 text-white p-4 text-center">
+      <p>&copy; 2023 Sentiment Analysis. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Bottom;
